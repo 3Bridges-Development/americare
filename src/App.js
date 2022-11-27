81be2b2939d63a43aec4a218f7a6bf4eeb78a3f4
nkleinmann <nekleinmann@gmail.com>
2022-11-27 17:09:47 -0500

    add react app and tailwind css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './components/navigation';
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
