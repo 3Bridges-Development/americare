cbe084390e5a4568ef4e28d093219a616779211f
nkleinmann <nekleinmann@gmail.com>
2023-02-11 12:42:23 -0500

    working example of contentful and added .env

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const query = `
+query {
+  mission(id: "56SMlz81KtgvWHdmU4fnKz") {
+    description
+  }
+}`
+
+const {REACT_APP_SPACE_ID, REACT_APP_CDA_TOKEN} = process.env
 
 function BodyInfo() {
+  let [description, setDescription] = useState("")
+
+  useEffect(() => {
+    window.fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}?`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${REACT_APP_CDA_TOKEN}`
+      },
+      body: JSON.stringify({ query }),
+    }).then(response => response.json())
+    .then(json => setDescription(json.data))
+  }, [])
+
+  if(!description) return <span>Loading...</span>
+
   return (
     <>
       <div className="body-container flex justify-evenly max-md:flex-col items-center mt-5">
         <div className="mission-wrapper">
           <h2 className="text-2xl text-center font-bold">Mission</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p>{description.mission.description}</p>
           <p>Lorem ipsum dolor sit amet.</p>
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
