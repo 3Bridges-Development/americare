9cfbc325587db54e91af875860d0318e9312e800
ambertrand <alex.m.bertrand@gmail.com>
2023-02-26 15:21:48 -0500

    mission text spacing

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -6,9 +6,15 @@ function BodyInfo() {
       <div className="body-container flex justify-evenly max-md:flex-col items-center mt-5">
         <div className="mission-wrapper">
           <h2 className="text-2xl text-center font-bold">Mission</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <p className="text-base m-3">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusantium nesciunt aliquam quisquam ducimus velit blanditiis
+            eveniet accusamus nostrum! Dolores non assumenda modi a, doloribus
+            nihil porro possimus. Assumenda placeat porro minima dignissimos
+            quisquam quia, enim corrupti saepe magni autem ut dolores corporis
+            perferendis architecto, explicabo recusandae vel reiciendis. Et,
+            non.
+          </p>
         </div>
         <div className="img-wrapper">
           <img
