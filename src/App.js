3340fce46e37dd37261be0d93c9f192003cc720e
ambertrand <alex.m.bertrand@gmail.com>
2023-02-26 18:27:48 -0500

    moving files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import Navigation from "./components/navigation";
-import BodyInfo from "./components/info";
-import FAQ from "./components/faq";
-import Footer from "./components/footer";
+import Navigation from "./components/layout/navigation";
+import Footer from "./components/layout/footer";
+import HomePage from "./views/HomeView";
+import AboutPage from "./views/AboutView";
 import "./App.css";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen text-center">
       <Navigation />
-      <div class="bg-fixed bg-cover" style={{height: "25rem", backgroundImage: "url(https://media.istockphoto.com/id/1130561533/photo/lebanese-doctor-standing-with-stethoscope-on-lebanon-flag-background-national-healthcare.jpg?s=612x612&w=0&k=20&c=ZXw5uNochGSky3VNSvnHmn0ZMIIWmx65wSh_k3fGa18=)"}}></div>
-      <BodyInfo />
-      <FAQ />
+      <div
+        class="bg-fixed bg-cover"
+        style={{
+          height: "25rem",
+          backgroundImage:
+            "url(https://media.istockphoto.com/id/1130561533/photo/lebanese-doctor-standing-with-stethoscope-on-lebanon-flag-background-national-healthcare.jpg?s=612x612&w=0&k=20&c=ZXw5uNochGSky3VNSvnHmn0ZMIIWmx65wSh_k3fGa18=)",
+        }}
+      ></div>
+      <HomePage />
+      <AboutPage />
       <Footer />
     </div>
   );
